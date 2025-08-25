7c4fb366368b14317aa56c67ec9dcf8c5b0b4f82
luizfelipe17 <luizfeliperabelo17@gmail.com>
2025-08-24 23:50:11 -0300

    COMIT

--- src/App.js
+++ src/App.js
@@ -17,11 +17,8 @@ import especialidadesFinanceiro from "./img/EspecialidadesFinanceiro.jpg"
 import inss from "./img/inss.png"
 import mei from "./img/mei.png"
 import simplesLucro from "./img/lucroPresumido.png"
-import { FiPhone } from "react-icons/fi";
-import { FiMail } from "react-icons/fi";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaRegClock } from "react-icons/fa";
-import { FiMapPin } from "react-icons/fi";
+import { FiMapPin, FiMail, FiPhone} from "react-icons/fi";
+import { FaWhatsapp, FaRegClock, FaIdCard, FaAddressCard, FaHome, FaHeart, FaFileInvoice, FaUsers, FaFileAlt} from "react-icons/fa";
 
 function App() {
   return (
@@ -46,10 +43,6 @@ function App() {
           <img className="logoTelaInicial" src={logo}/>
         </div>
 
-        <div className='buttonWhatsFixo'>
-          <a href='https://wa.me/+5531988997860'>FALAR COM O CONTADOR</a>
-        </div>
-
          <div className='objetivos'> 
           <div className = 'boxObjetivos'>
             <img src={missao}/>
@@ -70,138 +63,171 @@ function App() {
             </p></div>
         </div> 
 
-        <div className='servicos'>
-            <div id="simplesLucro">
-              <h1>Serviços e Soluções - Simples Nacional / Lucro Presumido<br></br><br></br></h1>
-              <p>•	Abertura de empresa<br></br>
-              •	Baixa de empresa<br></br>
-              •	Alterações Contratuais<br></br>
-              •	Transformação em Sociedade Limitada<br></br>
-              •	Transformação Sociedade Unipessoal Limitada<br></br>
-              •	Migrar de MEI para Micro Empresa.<br></br>
-              •	Emissão de Notas Fiscais Eletrônica e Notas Fiscais de Serviço Eletrônica<br></br>
-              •	Compensação Restituição ressarcimento de Créditos Tributários Federal<br></br>
-              •	Processos Digitais: Administrativo tributário Federal ou Municipal.<br></br></p>
-              <button>Abrir Empresa</button>
-            </div>
-            <img id = "simplesLucroimg" src={simplesLucro}/>
-            <div id="servicoSolucoes">
-              <img src={mei}/>
-              Serviços e Soluções – MEI / Pessoa Física <br></br><br></br>
+         <div className='servicos'>
+        <div id="simplesLucro">
+          <h1>Serviços e Soluções - Simples Nacional / Lucro Presumido<br></br><br></br></h1>
+          <p>•	Abertura de empresa<br></br>
+            •	Baixa de empresa<br></br>
+            •	Alterações Contratuais<br></br>
+            •	Transformação em Sociedade Limitada<br></br>
+            •	Transformação Sociedade Unipessoal Limitada<br></br>
+            •	Migrar de MEI para Micro Empresa.<br></br>
+            •	Emissão de Notas Fiscais Eletrônica e Notas Fiscais de Serviço Eletrônica<br></br>
+            •	Compensação Restituição ressarcimento de Créditos Tributários Federal<br></br>
+            •	Processos Digitais: Administrativo tributário Federal ou Municipal.<br></br></p>
+          <a id="buttonAbrirEmpresa">Abrir Empresa</a>
+        </div>
 
-                •	DIRPF<br></br>
-                •	GCAP<br></br>
-                •	CARNÊ LEÂO<br></br>
-                •	MENSALÃO<br></br>
-                •	MALHA FISCAL<br></br>
-                •	DITR <br></br>
-                •	CCIR<br></br>
-                •	Processos Digitais: Administrativo e Tributário Federal ou Municipal<br></br>
-                •	Distribuição de lucros na regra do MEI<br></br>
-                •	Confecção de Folha de pagamento para MEI<br></br>
-                •	Pedido de Restituição<br></br>
+        <img id="simplesLucroimg" src={simplesLucro} />
 
-            </div>
-            <div id="servicoPrevidenciario">  Serviços e Soluções - PREVIDENCIÁRIO – INSS<br></br><br></br>
+        <div id="servicoSolucoes">
 
-                  •	Aposentadoria<br></br>
-                  •	Auxilio Doença<br></br>
-                  •	Auxilio Acidente<br></br>
-                  •	Pensão por Morte<br></br>
-                  •	 Salário Maternidade <br></br>
-                  •	Análise e acerto de CNIS<br></br>
-                  •	Planejamento Previdenciário<br></br>
-                  •	Revisão/ recurso Administrativo. <br></br>       
-                  (TODOS ESSES SERVICOS PREVIDENCIARIO SERÁ PRESTADO SOMENTE NO ENDEREÇO DE CONTAGEM)
-                  <img src={inss}/>
+          <h1>Serviços e Soluções – MEI / Pessoa Física</h1> <br></br><br></br>
 
-            </div>
+          •	DIRPF<br></br>
+          •	GCAP<br></br>
+          •	CARNÊ LEÂO<br></br>
+          •	MENSALÃO<br></br>
+          •	MALHA FISCAL<br></br>
+          •	DITR <br></br>
+          •	CCIR<br></br>
+          •	Processos Digitais: Administrativo e Tributário Federal ou Municipal<br></br>
+          •	Distribuição de lucros na regra do MEI<br></br>
+          •	Confecção de Folha de pagamento para MEI<br></br>
+          •	Pedido de Restituição<br></br>
+
+        </div>
+
+        <img id="servicosSolucoesIMG" src={mei} />
+
+        <div id="servicoPrevidenciario">  <h1>Serviços e Soluções - PREVIDENCIÁRIO – INSS</h1><br></br><br></br>
+
+          •	Aposentadoria<br></br>
+          •	Auxilio Doença<br></br>
+          •	Auxilio Acidente<br></br>
+          •	Pensão por Morte<br></br>
+          •	 Salário Maternidade <br></br>
+          •	Análise e acerto de CNIS<br></br>
+          •	Planejamento Previdenciário<br></br>
+          •	Revisão/ recurso Administrativo. <br></br><br></br>
+          <h4>(TODOS ESSES SERVICOS PREVIDENCIARIO SERÁ PRESTADO SOMENTE NO ENDEREÇO DE CONTAGEM)</h4>
 
 
         </div>
 
-        <div className='aberturaEmpresas'>
+        <img id="servicoPrevidenciarioImg" src={inss} />
+      </div>
+
+      <div className='aberturaEmpresas'>
+        <div>
+          <h3>Abertura de empresa: documentos necessários</h3>
           <div className='boxAberturaEmpresas'>
-            
-            <h3>Abertura de empresa alguns documentos, tais como:</h3>
-            <p>RG, CPF, Comprovante de endereço, Certidão de casamento (se for casado), Cópia do IPTU
-              ou documentos que indique a inscrição municipal do imóvel em que será a sede da sua
-              empresa. Caso você decida ter sócios deverá providenciar os mesmos documentos de todos
-              eles. Algumas atividades ou situações podem exigir documentos adicionais.
-            </p>
-            <img src={aberturaEmpresas}/>
+            <div id = "boxAberturaEmpresaItens">
+              <FaIdCard size={40} />
+              <p>RG</p>
+            </div>
+            <div id = "boxAberturaEmpresaItens">
+              <FaAddressCard size={40} />
+              <p>CPF</p>
+            </div>
+            <div id = "boxAberturaEmpresaItens">
+              <FaHome size={40} />
+              <p>Comprovante de Endereço</p>
+            </div>
+            <div id = "boxAberturaEmpresaItens">
+              <FaHeart size={40} />
+              <p>Certidão de Casamento<br />(se casado)</p>
+            </div>
+            <div id = "boxAberturaEmpresaItens">
+              <FaFileInvoice size={40} />
+              <p>Cópia do IPTU ou<br />Inscrição Municipal</p>
+            </div>
+            <div id = "boxAberturaEmpresaItens">
+              <FaFileAlt size={40} />
+              <p>Documentos Adicionais<br />(se necessário)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='containerEspecialidades'>
+
+        <div className='tituloEspecialidas'><h1>Especialidades</h1></div>
+
+        <div className="containerCards">
+          <div className='boxEspecialidades'>
+            <img id='imgEspecialidades' src={especialidadesFinanceiro}></img>
+            <div className='textoHouver'>Execução impecável e sem erros das operações Financeiras da sua Empresa.</div>
+          </div>
+          <div className='boxEspecialidades'>
+            <img id='imgEspecialidades' src={parceriaEmpresarial}></img>
+            <div className='textoHouver'>Soluções completas parcerias de confiança em sua vida empresarial.</div>
+          </div>
+          <div className='boxEspecialidades'>
+            <img id='imgEspecialidades' src={baixoImposto}></img>
+            <div className='textoHouver'>Mantemos a sua empresa regularizada e pagando o mínimo de impostos realmente devidos e que a empresa esteja sempre em dia com suas obrigações.  </div>
+          </div>
+          <div className='boxEspecialidades'>
+            <img id='imgEspecialidades' src={estruturaContabil}></img>
+            <div className='textoHouver'>Nosso time de contadores ajudará você a escolher o regime tributário e toda organização da estrutura contábil da sua empresa.</div>
           </div>
         </div>
 
-        <div className='containerEspecialidades'>
 
-            {/* <h1>Especialidades</h1>  */}
-            <div className='boxEspecialidades'>
-              <img id='imgEspecialidades'src={especialidadesFinanceiro}></img>
-              <div className='textoHouver'>•	Execução impecável e sem erros das operações Financeiras da sua Empresa.</div>
-            </div>
-            <div className='boxEspecialidades'>
-              <img id='imgEspecialidades' src={parceriaEmpresarial}></img>
-              <div className='textoHouver'>•	Soluções completas parcerias de confiança em sua vida empresarial.</div>
-            </div>
-            <div className='boxEspecialidades'>
-              <img id='imgEspecialidades' src={baixoImposto}></img>  
-              <div className='textoHouver'>•	Mantemos a sua empresa regularizada e pagando o mínimo de impostos realmente devidos e que a empresa esteja sempre em dia com suas obrigações.  </div>
-            </div>
-            <div className='boxEspecialidades'>
-              <img id='imgEspecialidades' src={estruturaContabil}></img>
-              <div className='textoHouver'>•	Nosso time de contadores ajudará você a escolher o regime tributário e toda organização da estrutura contábil da sua empresa.</div>
-            </div>
-        </div>
- 
-        <div id='contato' className="contato">
-          <h1>Contatos</h1>
+      </div>
 
-              <a href='mailto:oscarcontabilidade@gmail.com?subject=Contato+via+Site+%E2%80%93+K.R.O.+Consultoria&body=Ol%C3%A1,++Estou+entrando+em+contato+atrav%C3%A9s+do+site+da+K.R.O.+Consultoria+para+tirar+algumas+d%C3%BAvidas+sobre+os+servi%C3%A7os+oferecidos.++Fico+no+aguardo+do+retorno.+Obrigado!'>
-              <img className="icones-contato" src={email}/>
-              </a>
+      <div id='contato' className="contato">
+        <h1>Contatos</h1>
 
-              <img className="icones-contato" src={telefone}/>
+        <a href='mailto:oscarcontabilidade@gmail.com?subject=Contato+via+Site+%E2%80%93+K.R.O.+Consultoria&body=Ol%C3%A1,++Estou+entrando+em+contato+atrav%C3%A9s+do+site+da+K.R.O.+Consultoria+para+tirar+algumas+d%C3%BAvidas+sobre+os+servi%C3%A7os+oferecidos.++Fico+no+aguardo+do+retorno.+Obrigado!'>
+          <img className="icones-contato" src={email} />
+        </a>
 
-              <a href='https://wa.me/+5531988997860'>
-              <img className="icones-contato" src={whats}/>
-              </a>
+        <img className="icones-contato" src={telefone} />
 
-        </div>
+        <a href='https://wa.me/+5531988997860'>
+          <img className="icones-contato" src={whats} />
+        </a>
 
-        <div id='sobreNos' className ='sobreNos'>
-          <img className='imgFundoSobreNos' src={fundoSobreNos}/> 
-          <p>Sobre Nós<br></br>A K.R.O Consultoria Empresarial Previdenciária Tributaria Ltda, conhecida como Oscar Contabilidade, possui mais de 25 anos de experiência no mercado, oferecendo serviços contábeis com ética, dedicação e foco na resolução de problemas.
+      </div>
+
+      <div id='sobreNos' className='sobreNos'>
+        <img className='imgFundoSobreNos' src={fundoSobreNos} />
+        <p>Sobre Nós<br></br>A K.R.O Consultoria Empresarial Previdenciária Tributaria Ltda, conhecida como Oscar Contabilidade, possui mais de 25 anos de experiência no mercado, oferecendo serviços contábeis com ética, dedicação e foco na resolução de problemas.
           Atuamos com empresas de diferentes portes e segmentos, sempre com um atendimento próximo e humanizado. Oferecendo um suporte completo, técnico e confiável em cada etapa.
-          Nosso trabalho vai além do cumprimento de obrigações fiscais. Atuamos de forma consultiva, buscando eficiência, economia e transparência, para que cada decisão seja tomada com segurança e visão de futuro. 
-          </p>
+          Nosso trabalho vai além do cumprimento de obrigações fiscais. Atuamos de forma consultiva, buscando eficiência, economia e transparência, para que cada decisão seja tomada com segurança e visão de futuro.
+        </p>
 
-          <img className='imgContador' src={contador}/>
-        </div>
+        <img className='imgContador' src={contador} />
+      </div>
+
+      <div className='buttonWhatsFixo'>
+        <a href='https://wa.me/+5531988997860' target='_blank'><FaWhatsapp/></a>
+      </div>
 
       <footer className='rodape'>
         <br></br>
         <div className='containerRodape'>
           <div id='enderecoRodape' className='boxRodape'>
-          <address>
-            K.R.O Consultoria Empresarial Previdenciaria Tributaria LTDA: 11.020.769/0001-98<br></br><br></br>
-            
-            <h3><FiMapPin /> Unidade Betim</h3>
-            R. Inconfidência, 510 - Centro, Betim<br></br>
-            MG, 32600-100<br></br><br></br>
+            <address>
+              K.R.O Consultoria Empresarial Previdenciaria Tributaria LTDA: 11.020.769/0001-98<br></br><br></br>
 
-            <h3><FiMapPin /> Unidade Contagem - "Somente Serviços Previdenciários"</h3>
-            Rua Moassy, 48 - Novo Eldorado, Contagem<br></br>
-            MG, 32341-170<br></br>
+              <h3><FiMapPin /> Unidade Betim</h3>
+              R. Inconfidência, 510 - Centro, Betim<br></br>
+              MG, 32600-100<br></br><br></br>
 
-          </address>
-        </div>
-          
-          <div id = 'contatoRodape' className='boxRodape'>
+              <h3><FiMapPin /> Unidade Contagem - "Somente Serviços Previdenciários"</h3>
+              Rua Moassy, 48 - Novo Eldorado, Contagem<br></br>
+              MG, 32341-170<br></br>
+
+            </address>
+          </div>
+
+          <div id='contatoRodape' className='boxRodape'>
             <h3>CONTATO</h3><br></br>
-            <a href='mailto:oscarcontabilidade@gmail.com?subject=Contato+via+Site+%E2%80%93+K.R.O.+Consultoria&body=Ol%C3%A1,++Estou+entrando+em+contato+atrav%C3%A9s+do+site+da+K.R.O.+Consultoria+para+tirar+algumas+d%C3%BAvidas+sobre+os+servi%C3%A7os+oferecidos.++Fico+no+aguardo+do+retorno.+Obrigado!'><FiMail /> OSCARCONTABILIDADE@GMAIL.COM</a><br></br>
-            <a href='https://wa.me/+5531988997860'><FaWhatsapp /> WHATSAPP: +55 (31) 98899-7860</a><br></br>
-            <a><FiPhone/> TELEFONE: (31) 3594-1294</a><br></br>
+            <a href='mailto:oscarcontabilidade@gmail.com?subject=Contato+via+Site+%E2%80%93+K.R.O.+Consultoria&body=Ol%C3%A1,++Estou+entrando+em+contato+atrav%C3%A9s+do+site+da+K.R.O.+Consultoria+para+tirar+algumas+d%C3%BAvidas+sobre+os+servi%C3%A7os+oferecidos.++Fico+no+aguardo+do+retorno.+Obrigado!' target='_blank'><FiMail /> OSCARCONTABILIDADE@GMAIL.COM</a><br></br>
+            <a href='https://wa.me/+5531988997860' target='_blank'><FaWhatsapp /> WHATSAPP: +55 (31) 98899-7860</a><br></br>
+            <a><FiPhone /> TELEFONE: (31) 3594-1294</a><br></br>
             <br></br>
           </div>
 
@@ -212,10 +238,11 @@ function App() {
           </div>
         </div>
 
-          <div id='copyright'>Copyright © 2025 - K.R.O Consultoria Empresaria Previdenciária Tributaria LTDA - Todos os direitos reservados.</div>
-        
+        <div id='copyright'>Copyright © 2025 - K.R.O Consultoria Empresaria Previdenciária Tributaria LTDA - Todos os direitos reservados.</div>
+
       </footer>
     </div>
+
   );
 }
 
